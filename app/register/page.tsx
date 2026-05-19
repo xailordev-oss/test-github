4164a11facebcc60d8ba2956465b0ca32193fe0b
xailordev-oss <xailordev@gmail.com>
2026-05-19 11:39:04 +0700

    first

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -9,7 +9,7 @@ export default function RegisterPage() {
 
         {/* TITLE */}
         <h1 className="text-3xl font-bold text-center text-gray-800">
-          create account 🚀
+          Create account 🚀
         </h1>
         <p className="text-center text-gray-500 mt-2 mb-6">
           Sign up to start shopping
